d34b4706afa2a9ca9f89810e06560bfac8986837
fabmav <fabrice.mavoungou@gmail.com>
2025-06-28 12:50:41 +0200

    update streamlit config

--- app_streamlit/config_streamlit.py
+++ app_streamlit/config_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-from functions_app_streamlit import *
+from app_streamlit.functions_app_streamlit import *
 import plotly.express as px
 from wikipedia import summary
 import os
